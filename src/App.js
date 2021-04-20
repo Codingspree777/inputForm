336d3e4b0596f44aae058973838b2f0360625c8c
Mike Huynh <mhuynh517@gmail.com>
2021-04-19 20:13:26 -0700

    moment, logics

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import moment from 'moment';
 import './App.css';
 
 import submitFormData from './submitFormData';
 
 function App() {
 
-  const [mapFields, setMapFields] = useState(
-    submitFormData.filter(field => !field.conditional)
-  );
+  const [inputFields, setMapFields] = useState(submitFormData);
+  const [dateValue, setDateValue] = useState('');
 
   const handleInputField = (e) => {
-    console.log(e.target.name, 'regular');
+    setMapFields(inputFields.map((field)=>{
+      if(field.name === e.target.name) {
+        return {
+          ...field,
+          textValue: e.target.value
+        }
+      }
+      return field;
+    }))
   }
-
+  
   const handleBirthDataField = (e) => {
-    console.log(e.target.value, 'birthday')
+    setDateValue(e.target.value)
+    const myDate = new Date(e.target.value);
+    console.log(inputFields[6].conditional.show_if(myDate), "birth")
   }
  
-  const InputFields = mapFields.map((field)=>{
-    return (
-      <div key={field.name}>
-        <label htmlFor={field.name}>{field.human_label}</label> <br/>
-        <input type={field.type} name={field.name} onChange={field.name === 'date_of_birth' ? handleBirthDataField : handleInputField}></input>
-      </div>
-    )
+  const InputFields = inputFields.map((field)=>{
+    if(!field.conditional) {
+      return (
+        <div key={field.name}>
+          <label htmlFor={field.name}>{field.human_label}</label> <br/>
+          { field.name !== 'date_of_birth' ? <input type={field.type} name={field.name} onChange={handleInputField}></input> : 
+            <input type={field.type} name={field.name} onChange={handleBirthDataField} min="1900-01-01" max={moment(new Date()).format("YYYY-MM-DD")} value={dateValue}></input> 
+          }
+        </div>
+      )
+    } else {
+      if(field.conditional.show_if(new Date(dateValue))) {
+        return (
+             <React.Fragment>
+               <label htmlFor={field.name}>{field.human_label}</label> <br/>
+               <input type={field.type} name={field.name}></input>
+             </React.Fragment>
+        )
+        
+      }
+    }
   });
 
   return (
