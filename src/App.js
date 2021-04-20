7cc07d04d0a86f0dba72779f2bb8c534838aed91
Mike Huynh <mhuynh517@gmail.com>
2021-04-20 10:55:28 -0700

    finish logic

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   const [dateValue, setDateValue] = useState('');
 
   const handleInputField = (e) => {
-    setMapFields(inputFields.map((field)=>{
-      if(field.name === e.target.name) {
+    setMapFields(inputFields.map((field) => {
+      if (field.name === e.target.name) {
         return {
           ...field,
           textValue: e.target.value
@@ -19,43 +19,62 @@ function App() {
       }
       return field;
     }))
-  }
-  
+  };
+
   const handleBirthDataField = (e) => {
-    setDateValue(e.target.value)
-    const myDate = new Date(e.target.value);
-    console.log(inputFields[6].conditional.show_if(myDate), "birth")
-  }
- 
-  const InputFields = inputFields.map((field)=>{
-    if(!field.conditional) {
+    setDateValue(e.target.value);
+    setMapFields(inputFields.map((field) => {
+      if (field.name === e.target.name) {
+        return {
+          ...field,
+          textValue: e.target.value
+        }
+      }
+      return field;
+    }))
+  };
+
+  const handleParentialConsent = (e) => {
+    setMapFields(inputFields.map((field)=>{
+      if(field.name === e.target.name) {
+        return {
+          ...field,
+          parential_consent: true
+        }
+      }
+      return field;
+    }))
+  };
+  
+  const InputFields = inputFields.map((field) => {
+    if (!field.conditional) {
       return (
         <div key={field.name}>
-          <label htmlFor={field.name}>{field.human_label}</label> <br/>
-          { field.name !== 'date_of_birth' ? <input type={field.type} name={field.name} onChange={handleInputField}></input> : 
-            <input type={field.type} name={field.name} onChange={handleBirthDataField} min="1900-01-01" max={moment(new Date()).format("YYYY-MM-DD")} value={dateValue}></input> 
+          <label htmlFor={field.name}>{field.human_label}</label> <br />
+          {field.name !== 'date_of_birth' ? <input type={field.type} name={field.name} onChange={handleInputField}></input> :
+            <input type={field.type} name={field.name} onChange={handleBirthDataField} min="1900-01-01" max={moment(new Date()).format("YYYY-MM-DD")} value={dateValue}></input>
           }
         </div>
       )
     } else {
-      if(field.conditional.show_if(new Date(dateValue))) {
+      if (field.conditional.show_if(new Date(dateValue))) {
         return (
-             <React.Fragment>
-               <label htmlFor={field.name}>{field.human_label}</label> <br/>
-               <input type={field.type} name={field.name}></input>
-             </React.Fragment>
+          <div key={field.name}>
+            <label htmlFor={field.name}>{field.human_label}</label> <br />
+            <input type={field.type} name={field.name} onClick={handleParentialConsent}></input>
+          </div>
         )
-        
+
       }
     }
   });
 
   return (
     <div className="App">
-    <form>
-      {InputFields}
-      <input type="submit"></input>
-    </form>
+      <form>
+        {InputFields}
+        <input type="submit"></input>
+      </form>
     </div>
   );
 }
